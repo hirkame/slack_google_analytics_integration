515e64c9ff276f96c3803875194d51d962ba57b1
Hiroki Kameyama <vjtsx.45afv@gmail.com>
2019-09-13 17:52:17 +0900

    Added prod.js on config folder

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  view_id: process.env.VIEW_ID,
+  slack_webhook_uri: process.env.SLACK_WEBHOOK_URI,
+  client_email: process.env.CLIENT_EMAIL,
+  private_key: process.env.PRIVATE_KEY
+};
